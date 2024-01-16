70c341a4ebb30f285bc9a0bd06812c8e98c390ea
ThinkMuk <b2000kang@gmail.com>
2024-01-16 23:00:23 +0900

    darkmode function implemented

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { useState } from "react";
 import TodoList from "./components/TodoList/TodoList";
 import Header from "./components/Header/Header";
+import { DarkModeProvider } from "./components/context/DarkModeContext";
 
 const filters = ["all", "active", "completed"];
 export default function App() {
   const [filter, setFilter] = useState(filters[0]);
   return (
     <>
-      <Header filters={filters} filter={filter} onFilterChange={setFilter} />
-      <TodoList filter={filter} />
+      <DarkModeProvider>
+        <Header filters={filters} filter={filter} onFilterChange={setFilter} />
+        <TodoList filter={filter} />
+      </DarkModeProvider>
     </>
   );
 }
